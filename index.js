df98d607e07fa8f07672a25a0172fa7b7ec276d0
ArwaYearub <153165419+ArwaYearub@users.noreply.github.com>
2024-02-12 15:26:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-function showYoungestStudent(students) { }
+var students = [
+    {
+        name: 'arwa',
+        age: 20
+    },
+    {
+        name: 'Nihad',
+        age: 33
+    },
+    {
+        name: 'Noor',
+        age: 23
+    }
+]
 
+function showYoungestStudent(students) { 
+    var temp = students[0];
+
+    for(var i=0; i<students.length;i++){
+        if(temp.age >= students[i].age){
+            temp = students[i];
+        }
+    }
+
+    console.log(temp.name) //prints the name of the youngest student
+}
+
+showYoungestStudent(students);
 module.exports = showYoungestStudent;
